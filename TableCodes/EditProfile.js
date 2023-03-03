6bfe25a8095e97827bf87e6ff7615330f50d8b0f
George Godwin <georgedaninno@gmail.com>
2023-03-03 12:30:08 +0100

    Change

--- TableCodes/EditProfile.js
+++ TableCodes/EditProfile.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const router = express.Router();
-const cloudinary = require('cloudinary');
+const cloudinary = require('cloudinary').v2;
 const {UserAuthentication} = require('../models');
 
 const {validationToken} = require("../JWT/JwebToken") 
 
-router.put("/editImage", validationToken, async (req,res) => {
-    const {newImage,id} = req.body;
-    await UserAuthentication.cloudinary.v2.uploader.destroy({UserImage:newImage},{where:{id:id}});
+cloudinary.config({ 
+  cloud_name: process.env.cloud_name, 
+  api_key: process.env.api_key, 
+  api_secret: process.env.api_secret
+});
+router.put('/editImage',validationToken, async (req,res) => {
+  const {newImage,id} = req.body;
+  try {
+    const public = await UserAuthentication.findOne({where:{id:id}});
+    const imageId = public.UserImage;
+    console.log(imageId);
+    await cloudinary.uploader.destroy(imageId);
+    
     await UserAuthentication.update({UserImage:newImage},{where:{id:id}});
-    res.json(newImage);
+    res.status(200).json({
+      status:true,
+      message:"uploaded"
+    })
+  } catch (error) {
+    
+  }
 })
+
+
+
 
 
 module.exports = router;
